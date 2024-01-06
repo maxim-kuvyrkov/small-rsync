2500b2e960addbe4d7daba2ef16cc4f73f257e2d
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2024-01-06 08:41:20 -0500

    Drop RAM-limited logic, convert to a normal backup script

--- small-rsync.sh
+++ small-rsync.sh
@@ -5,9 +5,12 @@ set -euf -o pipefail
 backup_dir="${1-/mnt/btrfs/snapshots}"
 backup_dir=$(cd "$backup_dir" && pwd)
 
+remote="macmini.root"
+remote_dir="/mnt/btrfs"
+
 # Subdirectory relative to $backup_dir to backup.
 # Mostly for testing on a subset of filesystem.
-#subdir="/home/maxim/bin/"
+#subdir="/@home/maxim/bin/"
 subdir="/"
 
 tmp_dir="$backup_dir/.backup"
@@ -29,19 +32,8 @@ cleanup_exit()
 	btrfs subvolume delete "$tmp_dir/$i"
     done
 
-    $rsh dir825 rsync -a --del /opt/ /mmc/opt/
-    #$rsh dir825 iptables -D INPUT -p tcp --dport 2222 -d 192.168.1.72 -j ACCEPT
-    #$rsh dir825 iptables -D INPUT -p tcp --dport 2222 -d 192.168.1.73 -j ACCEPT
-    $rsh dir825 pkill sshd
-    sleep 5
-    $rsh dir825 umount /opt
-    $rsh dir825 mount -o bind /mmc/opt /opt
-    $rsh dir825 /opt/sbin/fsck.ext2 -y -C 0 /dev/sda1
-    $rsh dir825 umount /opt
-    $rsh dir825 umount /mmc
-    sleep 5
-    $rsh dir825 rmmod ext2
-    $rsh -O exit dir825
+    $rcmd umount $remote_dir
+    $rcmd -O exit
 }
 
 trap cleanup_exit EXIT
@@ -53,12 +45,7 @@ exec sudo -i -u maxim ssh -S/tmp/ssh-%u-%r@%h:%p "$@"
 EOF
 chmod +x $rsh
 
-rsh2="$tmp_dir"/rsh2
-cat > $rsh2 <<'EOF'
-#!/bin/sh
-exec sudo -i -u maxim ssh -p2222 -S/tmp/ssh-%u-%r@%h:%p "$@"
-EOF
-chmod +x $rsh2
+rcmd="$rsh $remote"
 
 cleanup=false
 if ! diff -q "$tmp_dir"/started "$tmp_dir"/finished; then
@@ -69,36 +56,13 @@ fi
 
 check_contents=false
 
-$rsh dir825 insmod ext2 || true
-$rsh dir825 mount /dev/sda1 /opt
-
-$rsh dir825 /opt/bin/sed -i -e "/^sshd/d" /tmp/etc/passwd
-echo "sshd:x:111:111:SSHD:/:/bin/false" | $rsh dir825 tee -a /tmp/etc/passwd
-$rsh dir825 /opt/sbin/sshd -p 2222
-#$rsh dir825 iptables -I INPUT 7 -p tcp --dport 2222 -d 192.168.1.72 -j ACCEPT
-#$rsh dir825 iptables -I INPUT 7 -p tcp --dport 2222 -d 192.168.1.73 -j ACCEPT
-
-cat <<'EOF' | $rsh2 dir825 sh -c "cat > /opt/etc/e2fsck.conf"
-[scratch_files]
-directory = /opt/e2fsck
-EOF
-$rsh2 dir825 rm -rf /opt/e2fsck/
-$rsh2 dir825 mkdir /opt/e2fsck
-$rsh2 dir825 /opt/sbin/fsck.ext2 -y -C 0 /dev/sda2 || check_contents=true
-$rsh2 dir825 mount /dev/sda2 /mmc
-
-cat <<'EOF' | $rsh dir825 sh -c "cat > /opt/bin/myrsync"
-#!/bin/sh
-echo 1000 > /proc/$$/oom_score_adj
-exec rsync "$@"
-EOF
-$rsh2 dir825 chmod +x /opt/bin/myrsync
+$rcmd mount $remote_dir
 
 date > "$tmp_dir"/started
 
 # Walk through snapshots named "@<subdir>.<date>T<time>" starting with
 # the oldest snapshots first.  Create a copy of the oldest snapshot, which
-# will be rsync'ed to dir825.
+# will be rsync'ed to $remote.
 declare -A done_subdirs
 for i in $(cd "$backup_dir"; find . -maxdepth 1 -type d ! -path . \
 	       | sed -e "s#^\./##" | sort -t. -k2); do
@@ -116,30 +80,24 @@ done
 
 cd "$tmp_dir"
 
+cleanup_opts="--dry-run"
 if $cleanup; then
-    # --delete-missing-args deletes remote directories, which do not exist
-    # locally.
-    # --existing delays transfer of new files till the main sync below.
-    # --ignore-existing delays update of files till the main sync below.
-    $rsh2 dir825 "cd /mmc$subdir; find -print0" \
-	| parallel --recend '\0' -0 --pipe -j1 -u --block 1M \
-		   rsync --delete --delete-missing-args --existing \
-		   --ignore-existing \
-		   -0 -aP --numeric-ids --files-from=- \
-		   -e $rsh2 --rsync-path=myrsync \
-		   ".$subdir" "dir825:/mmc$subdir" || true
+    cleanup_opts=""
 fi
+# --existing delays transfer of new files till the main sync below.
+# --ignore-existing delays update of files till the main sync below.
+rsync $cleanup_opts \
+    --delete --existing --ignore-existing \
+    -aP --numeric-ids -e $rsh \
+    ".$subdir" "$remote:$remote_dir$subdir" > cleanup.log || true
 
-rsync_cleanup_opts=""
+check_opts=""
 if $check_contents; then
-    rsync_cleanup_opts="--ignore-times"
+    check_opts="--ignore-times"
 fi
 
-find -type f -print0 \
-    | parallel --recend '\0' -0 --pipe -j1 -u --block 1M \
-	       rsync $rsync_cleanup_opts \
-	       -0 -aP --numeric-ids --files-from=- \
-	       -e $rsh2 --rsync-path=myrsync \
-	       ".$subdir" "dir825:/mmc$subdir"
+rsync $check_opts \
+      -aP --numeric-ids -e $rsh \
+      ".$subdir" "$remote:$remote_dir$subdir"
 
 cp "$tmp_dir"/started "$tmp_dir"/finished
